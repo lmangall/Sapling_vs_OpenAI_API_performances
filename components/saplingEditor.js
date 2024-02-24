5aaa2869623698c087706c662460602dbcdc38a0
lmangall <l.mangallon@gmail.com>
2024-02-24 12:49:48 +0100

    changing spaling settings

--- components/saplingEditor.js
+++ components/saplingEditor.js
@@ -10,6 +10,7 @@ function SaplingEditor() {
         editPathname: '/api/v1/edits',
         statusBadge: true,
         mode: 'dev',
+        lang: 'fr',
       });
 
       const editor = document.getElementById('sapling-editor');
